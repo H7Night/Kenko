adafe2fd5df54ef74d04ac9bf557285f05bb135f
H7Night <h7night@gmail.com>
2026-08-06 15:01:23 +0800

    chore: 全部 Kotlin 源文件追加 GPL-3.0 版权声明
    - 175 个已有上游版权头的文件追加 Copyright (C) 2026 H7Night <h7night@gmail.com>
    - 4 个无头文件（含 fork 新建的 ConfirmDialog/EmptyState/Toast 等）添加完整 GPL-3.0 版权头

--- app/src/androidTest/kotlin/com/looker/kenko/RepositoryTest.kt
+++ app/src/androidTest/kotlin/com/looker/kenko/RepositoryTest.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2025. LooKeR & Contributors
+ * Copyright (C) 2026 H7Night <h7night@gmail.com>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
